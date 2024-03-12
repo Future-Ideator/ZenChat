8e33939506cbd16be8ea784c45b87e1ecc20dad4
Shiwarne-MBiz <103165392+Shiwarne-Silva@users.noreply.github.com>
2024-03-13 03:35:17 +0530

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -17,7 +17,30 @@ export const signup = async (req, res) => {
     //HASH PASSWORD HERE
 
     //https://avatar-placeholder.iran.liara.run/
-  } catch (error) {}
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+    const newUser = new User({
+      fullName,
+      username,
+      password,
+      gender,
+      profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+    });
+
+    await newUser.save();
+
+    res.status(201).json({
+      _id: newUser._id,
+      fullName: newUser.fullName,
+      username: newUser.username,
+      profilePic: newUser.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in Signup Controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const login = async (req, res) => {
