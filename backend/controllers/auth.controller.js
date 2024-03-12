affa9599ebab8dcb23c844e4968ac854b7645245
Shiwarne-MBiz <103165392+Shiwarne-Silva@users.noreply.github.com>
2024-03-13 02:20:05 +0530

    update backend
    controllers folder and auth controller is created

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (rq, res) => {
+  console.log("signupUser");
+};
+
+export const login = (rq, res) => {
+  console.log("loginUser");
+};
+
+export const logout = (rq, res) => {
+  console.log("logoutUser");
+};
