0ecd2971d0a1dba5eb315392d2ca5c3128dc5cfd
Shiwarne-MBiz <103165392+Shiwarne-Silva@users.noreply.github.com>
2024-03-13 03:26:56 +0530

    database integration

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,29 @@
-export const signup = (rq, res) => {
-  console.log("signupUser");
+import User from "../models/user.model.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: "Passwords don't Match" });
+    }
+
+    const user = await User.findOne({ username });
+
+    if (user) {
+      return res.status(400).json({ error: "Username already exists" });
+    }
+
+    //HASH PASSWORD HERE
+
+    //https://avatar-placeholder.iran.liara.run/
+  } catch (error) {}
 };
 
-export const login = (rq, res) => {
+export const login = async (req, res) => {
   console.log("loginUser");
 };
 
-export const logout = (rq, res) => {
+export const logout = async (req, res) => {
   console.log("logoutUser");
 };
--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  //root route http://localhost:5000/
-  res.send("Server is Started");
-});
+dotenv.config();
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
+// app.get("/", (req, res) => {
+//   root route http://localhost:5000/
+//   res.send("Server is Started");
+// });
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on Port ${PORT}`);
+});
